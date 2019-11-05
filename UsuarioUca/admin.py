6cdf632842459b43e44600904ffaf1304444a18c
Juan Enrique Aicardo García <salkerps3@gmail.com>
2019-11-05 23:06:39 +0100

    [ADD] Custom user model
    First commit of custom user model with some bugs

--- UsuarioUca/admin.py
+++ UsuarioUca/admin.py
@@ -1,3 +1,20 @@
+# users/admin.py
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import UsuarioUca, Profesor, PASS, Estudiante
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = UsuarioUca
+    list_display = ['email', 'username']
+
+
+admin.site.register(UsuarioUca, CustomUserAdmin)
+admin.site.register(Profesor)
+admin.site.register(PASS)
+admin.site.register(Estudiante)
--- UsuarioUca/models.py
+++ UsuarioUca/models.py
@@ -1,3 +1,36 @@
+# users/models.py
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class UsuarioUca(AbstractUser):
+    # add additional fields in here
+    dni = models.CharField(max_length=9, blank=False, null=False)
+    egresado = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        db_table = 'auth_user'
+
+
+class PASS(models.Model):
+    class Meta:
+        verbose_name_plural = "Pass"
+
+    user = models.OneToOneField(UsuarioUca, on_delete=models.PROTECT, primary_key=True)
+
+
+class Estudiante(models.Model):
+    user = models.OneToOneField(UsuarioUca, on_delete=models.PROTECT, primary_key=True)
+    curso_max = models.IntegerField(blank=False, null=False)
+
+
+class Profesor(models.Model):
+    class Meta:
+        verbose_name_plural = "Profesores"
+
+    user = models.OneToOneField(UsuarioUca, on_delete=models.PROTECT, primary_key=True)
+    permanente = models.BooleanField(default=False)
+    doctor = models.BooleanField(default=False)
