7e73e179df723e0d538f61828b22316295bd8669
Mik9531 <miguelafannn@gmail.com>
2019-11-09 13:49:15 +0100

    DNI con texto de ejemplo

--- UsuarioUca/forms.py
+++ UsuarioUca/forms.py
@@ -4,13 +4,21 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import UsuarioUca
 
 
+
+
 class CustomUserCreationForm(UserCreationForm):
+    dni = forms.CharField(label='Dni', max_length=9, widget=forms.TextInput(attrs={"placeholder": "Ej:32085090K"}))
     class Meta:
         model = UsuarioUca
         fields = '__all__'
+
+
 
 
 class CustomUserChangeForm(UserChangeForm):
+
     class Meta:
         model = UsuarioUca
         fields = '__all__'
+
+
