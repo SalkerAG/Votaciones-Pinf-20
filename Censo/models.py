dbd1eec04e6627b9ece413b1761ff18c689fe9fd
guilogar <jdkdejava@gmail.com>
2019-11-20 12:17:40 +0100

    Resumen sobre creacion de funcionalidades basicas de censo, falta crear la logica robusta detras.

--- Censo/models.py
+++ Censo/models.py
@@ -1,17 +1,6 @@
 from django.db import models
 
 
-class UsuarioUcausuariouca(models.Model): # Clase para tomar los valores de la tabla UsuarioUca_usuariouca
-    first_name = models.CharField(max_length=30) # first_name registro de la tabla UsuarioUca_usuariouca
-    last_name = models.CharField(max_length=150) # last_name registro de la tabla UsuarioUca_usuariouca
-
-    class Meta: # esta clase nos permite poder definir la base datos asociada, nombre de la tabla
-        managed = False # falso = no crea tabla, true = crearia una tabla con los registros de arriba.
-        db_table = 'UsuarioUca_usuariouca' # Nombre de la tabla asociada al modelo.
-
-#def __str__(self):
-    #return self.first_name
-
-class Post(models.Model):
-    nombre = models.CharField(max_length=200)
-    apellido = models.CharField(max_length=200)
+class Censo(models.Model):
+    id_usuario = models.CharField(max_length=8, blank=False, null=False, default=32085090, unique=True)
+    id_votacion = models.IntegerField()
--- Censo/views.py
+++ Censo/views.py
@@ -1,13 +1,52 @@
 from django.shortcuts import render,HttpResponse
-from .models import UsuarioUcausuariouca # aqui importamos nuestra tabla a views
+from django.views.decorators.csrf import csrf_protect
+from UsuarioUca.models import UsuarioUca # aqui importamos nuestra tabla a views
+import sys
+from random import seed
+from random import random
+from .models import Censo
 
 
+@csrf_protect
 def Censo(request): # request este parametro es la peticion que ha pedido el usuario
-    usuario = UsuarioUcausuariouca.objects.all() # metemos dichos campos en usuario
-    return render(request,"Censo/censo.html", {'usuario': usuario}) # devolvemos los usuarios al template de nuestra tabla
+    #seed(1)
+    #u = UsuarioUca(
+    #    nif = '12345678',
+    #    egresado = True,
+    #    email = str(random()) + '@gmail.com'
+    #)
+    #u.save()
+    usuarios = UsuarioUca.objects.all() # metemos dichos campos en usuario
+    return render(request,"Censo/censo.html", {
+        'usuario': usuarios
+    }) # devolvemos los usuarios al template de nuestra tabla
 
-def post(request, id):
-    post = Post.objects.get(id = id)
-    return render(request, "Censo/censo.html", {'posts': posts})
+def guardarCenso(request, idVotacion):
+    
+    # Para obtener los nif de los usuarios que pueden votar en una votacion
+    # usuariosVotacionUca = Censo.objects.get(id_votacion=idVotacion)
+    
+    # Para obtener los atributos de los usuarios que pueden votar en una votacion
+    for uvc in usuariosVotacionUca:
+        usuarioUca = UsuarioUca.objects.get(nif = uvc.id_usuario)
+
+    if request.method=='POST':
+        if request.POST['usuariosVotacion']:
+            for nif in request.POST['usuariosVotacion']:
+                c = Censo(
+                    id_usuario = nif,
+                    id_votacion = idVotacion
+                )
+                c.save()
+
+    nombre_usuario = ''
+    #print('Value %s' % (nombre_usuario) )
+        #for key, value in request.POST.items():
+        #    print('Key: %s' % (key) )
+        #    print('Value %s' % (value) )
+    
+    # post = Post.objects.get(id = id)
+    
+    return render(request, "Censo/censo.html", {'posts': []})
 
 
