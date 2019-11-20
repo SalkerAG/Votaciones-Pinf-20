074de5679b42901952da6aefdad2bfed66c04c3d
Paulo <hernandezsanchezpaulo@gmail.com>
2019-11-20 16:08:10 +0100

    update votaciones-models

--- VotacionesUca/views.py
+++ VotacionesUca/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request,'VotacionesUca/home.html')
+
+def crearVotacion(request):
+    return render(request,'VotacionesUca/CrearVotacion.html')
 
