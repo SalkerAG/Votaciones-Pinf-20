671d220c90293d93dc72cca40f0e28bddec432e8
Paulo <hernandezsanchezpaulo@gmail.com>
2019-12-11 18:17:42 +0100

    cambio vistas

--- VotacionesUca/forms.py
+++ VotacionesUca/forms.py
@@ -3,12 +3,8 @@ from django.forms import ModelForm
 
 from django.core.exceptions import ValidationError
 from django import forms
-<<<<<<< Updated upstream
 from .views import CrearVotacionView
 from .models import ProcesoElectoral, Pregunta, Votacione, Eleccion, Opciones
-=======
-from .views import Votacione,Pregunta,ProcesoElectoral,Opciones
->>>>>>> Stashed changes
 
 def positivo(value):
    if value<=0:
@@ -31,19 +27,19 @@ class FormProcesoElectoral(forms.ModelForm):
 
 class PreguntaForm(forms.ModelForm):
     enunciado=forms.CharField(max_length=50,required=True)
-    #opciones=forms.ManyToManyField(Opciones())
+    opciones=forms.ManyToManyField(Opciones())
 
     class Meta:
         model=Pregunta
         fields='__all__'
 
 class VotacioneForm(forms.ModelForm):
-    #proceso=FormProcesoElectoral()
-    #pregunta=PreguntaForm()
-    #esPresencial=forms.BooleanField(label='Proceso Electoral presencial',initial=False)
-    #votoRectificable=forms.BooleanField(label='Voto rectificable',initial=False)
-    #tipoVotacion=forms.ChoiceField(label='Tipo de votacion',choices=c)
-    #maxElector=forms.IntegerField(label='Numero maximo de electores',validators=[positivo],min_value=0)
+    proceso=FormProcesoElectoral()
+    pregunta=PreguntaForm()
+    esPresencial=forms.BooleanField(label='Proceso Electoral presencial',initial=False)
+    votoRectificable=forms.BooleanField(label='Voto rectificable',initial=False)
+    tipoVotacion=forms.ChoiceField(label='Tipo de votacion',choices=c)
+    maxElector=forms.IntegerField(label='Numero maximo de electores',validators=[positivo],min_value=0)
 
     class Meta:
         model=Votacione
