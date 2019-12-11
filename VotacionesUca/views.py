30b352a713743f18d1e4cfa7d1ea830e7286a3f7
SergioRuizPino <49683335+SergioRuizPino@users.noreply.github.com>
2019-12-12 00:13:17 +0100

    Add files via upload
    Añadidas url y vistas de votacion simple y compleja.

--- VotacionesUca/views.py
+++ VotacionesUca/views.py
@@ -1,6 +1,20 @@
 from django.views.generic.base import TemplateView
 from .models import ProcesoElectoral, Pregunta, Votacione, Eleccion, Opciones
 
+
 class CrearVotacionView(TemplateView):
     template_name = "CrearVotacion.html"
-    return
+
+
+
+def votacion(request,op):
+	if(op==0):#votacion simple carga vista simple
+		template_name = "votacionSimple.html"
+	else:
+		template_name = "votacionCompleta.html"
+
+	
+
+
+
+		
