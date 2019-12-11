dac9c9a8d2a2c2dee58b46045da7e0ad7e25129a
isaac <is.acejopena@alum.uca.es>
2019-12-11 16:21:31 +0100

    [ADD]
    added import-export features to Censo

--- Censo/urls.py
+++ Censo/urls.py
@@ -5,9 +5,9 @@ from .views import CensoCreate, CensoDetailView
 from django.urls import reverse_lazy
 
 urlpatterns = [
-    path('censo/', CensoCreate.as_view(template_name="Censo/censo.html"), name="createCenso"),
-    path('censo/<int:pk>', CensoDetailView.as_view(template_name="Censo/censo.html"), name="DetailCenso"),
+    path('crearcenso/', CensoCreate.as_view(), name="censo_create"),
+    # path('censo/<int:pk>', CensoDetailView.as_view(template_name="Censo/censo.html"), name="DetailCenso"),
     # path('censo/', Censo),# indicamos la ruta para acceder al censo y la vista Censo
-    
+
     path('admin/', admin.site.urls),
 ]
