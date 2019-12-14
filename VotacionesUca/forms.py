721a76274d4afbd9d4bc5ec292f9584c76bd782f
Paulo <hernandezsanchezpaulo@gmail.com>
2019-12-14 21:08:00 +0100

    Implementado el diseño en las vistas

--- VotacionesUca/forms.py
+++ VotacionesUca/forms.py
@@ -1,77 +1,50 @@
-# users/forms.py
+#users/forms.py
 from django.forms import ModelForm
 from django.core.exceptions import ValidationError
 from django import forms
 from django.utils import timezone
 from datetime import datetime
-from .models import ProcesoElectoral, Pregunta, Votacione, Eleccion, Opciones
-
+from .models import ProcesoElectoral, Pregunta, Votacione, Eleccion
 
 def positivo(value):
-    if value <= 0:
-        raise ValidationError('El número debe ser mayor que 0.')
+   if value<=0:
+      raise ValidationError('El número debe ser mayor que 0.')
 
-
-c = [('0', 'Simple'), ('1', 'Compleja')]
-
+c=[('0','Simple'),('1','Compleja')]
 
 class DateTimeInput(forms.DateTimeInput):
-    input_type = 'date'
-
+    input_type = 'datetime-local'
+    attrs={}
 
 class ProcesoElectoralForm(forms.ModelForm):
-    create_at = forms.DateTimeField()
-    update_at = forms.DateTimeField()
-    esConsulta = forms.BooleanField(
-        widget=forms.CheckboxInput(attrs={'class': 'custom-control-input', 'id': 'customCheck1'}))
-    fechaInicio = forms.DateTimeField(label='Fecha Fin del proceso electoralsdsds', required=True, widget=DateTimeInput)
-    fechaFin = forms.DateTimeField(label='Fecha Fin del proceso electoral', required=True, widget=DateTimeInput)
-    nombreFicheroCenso = forms.CharField(required=True)
+    create_at=forms.DateTimeField() 
+    update_at=forms.DateTimeField()
+    esConsulta=forms.BooleanField()
+    fechaInicio=forms.DateTimeField(required=True, widget=DateTimeInput)
+    fechaFin=forms.DateTimeField(required=True, widget=DateTimeInput)
+    nombreFicheroCenso=forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder' : 'Nombre del Censo'}),required=True)
 
     class Meta:
-        model = ProcesoElectoral
-        fields = '__all__'
+        model=ProcesoElectoral
+        fields='__all__'
 
-
-class OpcionesForm(forms.ModelForm):
-    enunciado = forms.CharField()
-
-    class Meta:
-        model = Opciones
-        fields = '__all__'
-
-
-class PreguntaForm(OpcionesForm):
-    enunciado = forms.CharField(max_length=50, required=True)
-    opciones = OpcionesForm
+class PreguntaForm(forms.ModelForm):
+    enunciado=forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder' : 'Pregunta para la votacion'}))
+    opciones = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder' : 'Opciones a responder'}))
 
     class Meta:
-        model = Pregunta
-        fields = '__all__'
-
+        model=Pregunta
+        fields='__all__'
 
 class VotacioneForm(ProcesoElectoralForm, PreguntaForm):
-    proceso = ProcesoElectoralForm()
-    pregunta = PreguntaForm()
-    nombreVotacion = forms.CharField(
-        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Título de la votación'}))
-    esPresencial = forms.BooleanField(label='Proceso Electoral presencial', initial=False)
-    votoRectificable = forms.BooleanField(label='Voto rectificable', initial=False)
-    tipoVotacion = forms.ChoiceField(label='Tipo de votacion', choices=c)
-    maxElector = forms.IntegerField(label='Numero maximo de electores', validators=[positivo], min_value=0)
+    proceso=ProcesoElectoralForm()
+    pregunta=PreguntaForm()
+    nombreVotacion=forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder' : 'Título de la votación'}))
+    esPresencial=forms.BooleanField()
+    votoRectificable=forms.BooleanField()
+    tipoVotacion=forms.ChoiceField(label='Tipo de votacion',choices=c)
+    maxElector=forms.IntegerField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder' : 'Escribe el número máximo de respuestas'}), validators=[positivo],min_value=0)
 
     class Meta:
-        model = Votacione
-        fields = '__all__'
-
-
-class favor(forms.favor):
-    vF = forms.CharField(label='aFavor', max_length=1)
-
-
-class contr(forms.contr):
-    vC = forms.CharField(label='enContra', max_length=1)
-
-
-class abst(forms.abst):
-    vA = forms.CharField(label='abstencion', max_length=1)
+        model=Votacione
+        fields='__all__'
