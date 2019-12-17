443dc6dbe6f0ea2bef56f3d0ae5bb1fa390bb117
Juan Enrique <salkerps3@gmail.com>
2019-12-17 19:58:41 +0100

    [ADD]Censo model and Votation model

--- Censo/models.py
+++ Censo/models.py
@@ -1,10 +1,16 @@
+# users/models.py
+
 from django.db import models
 from UsuarioUca.models import UsuarioUca
+from VotacionesUca.models import ProcesoElectoral, Pregunta
 from django.urls import reverse_lazy
+
 
 
 class Censo(models.Model):
     usuario = models.ManyToManyField(UsuarioUca,  blank=False, null=False)
+    proceso = models.OneToOneField(ProcesoElectoral, on_delete=models.CASCADE)
+    pregunta = models.OneToOneField(Pregunta, on_delete=models.PROTECT)
 
     def get_absolute_url(self):
             return reverse_lazy('DetailCenso',  args=[str(self.id_votacion)])
